a259d2931b38c7f09cc3c2cfe0dd55a89577bf2f
Hugo Pereira <hugop868@gmail.com>
2025-08-04 23:29:09 -0300

    border

--- src/components/LandingPage/SignUpSection.tsx
+++ src/components/LandingPage/SignUpSection.tsx
@@ -3,40 +3,54 @@ import React from 'react';
 const SignUpSection: React.FC = () => {
   return (
     <section className="w-[1280px] flex justify-center items-center h-[695px] bg-black text-white mx-auto">
-      <div className="border border-red-500 bg-black mx-auto grid md:grid-cols-2 p-20 items-center overflow-hidden rounded-[8px]"
-        style={{ backgroundImage: 'url(/lp/fundo2last.png)', backgroundSize: 'cover', backgroundPosition: 'center' }}
+      {/* Gradient border wrapper */}
+      <div
+        className="p-[2px] rounded-[8px]"
+        style={{
+          background:
+            'linear-gradient(90deg, #002C45 0%, #F45111 35%, #199FEC 70%, #002C45 100%)',
+        }}
       >
-        {/* Form */}
-        <div className="space-y-20">
-          <h2 className="font-semibold text-[56px] leading-[64px] tracking-[-2.24px]">
-            Join Our Party
-            <br />
-            Sign Up For Updates
-          </h2>
-          <form className="space-y-4">
-            <input
-              type="text"
-              placeholder="Full Name"
-              className="w-[448px] px-4 py-3 rounded-[8px] bg-[rgba(255,255,255,0.15)]  text-white placeholder-gray-500 focus:outline-none"
-            />
-            <input
-              type="email"
-              placeholder="Email"
-              className="w-[448px] rounded-[8px] px-4 py-3 bg-[rgba(255,255,255,0.15)]  text-white placeholder-gray-500 focus:outline-none"
-            />
-            <button className="w-[448px] roudend-full bg-white text-black text-[16px] px-4 py-3 rounded-full">
-              Subscribe
-            </button>
-          </form>
-        </div>
-        <div className='w-[798px] h-[532px]'
+        <div
+          className="bg-black mx-auto grid md:grid-cols-2 p-20 items-center overflow-hidden rounded-[6px]"
           style={{
-            backgroundImage: 'url(/lp/fundo1last.png)',
+            backgroundImage: 'url(/lp/fundo2last.png)',
             backgroundSize: 'cover',
             backgroundPosition: 'center',
           }}
         >
-
+          {/* Form */}
+          <div className="space-y-20">
+            <h2 className="font-semibold text-[56px] leading-[64px] tracking-[-2.24px]">
+              Join Our Party
+              <br />
+              Sign Up For Updates
+            </h2>
+            <form className="space-y-4">
+              <input
+                type="text"
+                placeholder="Full Name"
+                className="w-[448px] px-4 py-3 rounded-[8px] bg-[rgba(255,255,255,0.15)]  text-white placeholder-gray-500 focus:outline-none"
+              />
+              <input
+                type="email"
+                placeholder="Email"
+                className="w-[448px] rounded-[8px] px-4 py-3 bg-[rgba(255,255,255,0.15)]  text-white placeholder-gray-500 focus:outline-none"
+              />
+              <button className="w-[448px] roudend-full bg-white text-black text-[16px] px-4 py-3 rounded-full">
+                Subscribe
+              </button>
+            </form>
+          </div>
+          <div
+            className="w-[798px] h-[532px]"
+            style={{
+              backgroundImage: 'url(/lp/fundo1last.png)',
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+            }}
+          ></div>
+          {/* End of grid content */}
         </div>
       </div>
     </section>
