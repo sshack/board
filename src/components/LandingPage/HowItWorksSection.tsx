e53be22e5ceb0d92850aab28173c9aaa53ca7466
Hugo Pereira <hugop868@gmail.com>
2025-08-12 00:34:01 -0300

    asd

--- src/components/LandingPage/HowItWorksSection.tsx
+++ src/components/LandingPage/HowItWorksSection.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const HowItWorksSection: React.FC = () => {
   return (
-    <section className="w-full bg-black text-white px-6 md:px-16 py-16 space-y-6">
+    <section className="w-full bg-cover bg-center flex flex-col items-center justify-center gap-0 md:gap-8 relative overflow-hidden max-w-[1440px]">
       <div className="flex flex-col space-y-2 items-center">
         <h2 className="text-[56px] md:text-4xl font-bold text-center leading-[64px]">
           How Does This Work?
@@ -11,21 +11,21 @@ const HowItWorksSection: React.FC = () => {
           ConQuesting is easy. Follow these three steps.
         </p>
       </div>
-      <div className="flex flex-col space-y-4 items-center">
+      <div className="flex flex-col space-y-4 items-center w-full !bg-pink-800">
         <div className="flex flex-col md:flex-row justify-between items-stretch w-full max-w-[1064px]">
-          {/* Bloco da esquerda */}
           <div
             className="h-[440px] w-full md:w-[50%] flex justify-center items-center"
             style={{
-              backgroundImage: "url(/lp/map.png)",
-              backgroundSize: "cover",
-              backgroundPosition: "center",
+              backgroundImage: 'url(/lp/map.png)',
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
             }}
+          ></div>
+
+          <div
+            className="h-[440px] w-full md:w-[50%] flex flex-col justify-center items-start bg-transparent p-8"
+            style={{ background: 'rgba(25, 159, 236, 0.15)' }}
           >
-          </div>
-          {/* Bloco da direita */}
-          <div className="h-[440px] w-full md:w-[50%] flex flex-col justify-center items-start bg-transparent p-8"
-            style={{background: "rgba(25, 159, 236, 0.15)"}}>
             <h3 className="text-white text-[44px] font-bold mb-2 leading-tight">
               Receive a Mystery <br /> Quest Scroll
             </h3>
@@ -40,26 +40,28 @@ const HowItWorksSection: React.FC = () => {
       </div>
       <div className="flex flex-col space-y-4 items-center">
         <div className="flex flex-col md:flex-row justify-between items-stretch w-full max-w-[1064px]">
-            {/* Bloco da direita */}
-            <div className="h-[440px] w-full md:w-[50%] flex flex-col justify-center items-start bg-transparent p-8"
-              style={{background: "rgba(25, 159, 236, 0.15)"}}>
-              <h3 className="text-white text-[44px] font-bold mb-2 leading-tight">
-                Complete your quest
-              </h3>
-              <p className="text-[rgba(255,255,255,0.5)] text-base mb-6 leading-[28px]">
-               Usually by snapping a picture of a specific cosplayer—with their consent, of course
-              </p>
-            </div>
+          {/* Bloco da direita */}
+          <div
+            className="h-[440px] w-full md:w-[50%] flex flex-col justify-center items-start bg-transparent p-8"
+            style={{ background: 'rgba(25, 159, 236, 0.15)' }}
+          >
+            <h3 className="text-white text-[44px] font-bold mb-2 leading-tight">
+              Complete your quest
+            </h3>
+            <p className="text-[rgba(255,255,255,0.5)] text-base mb-6 leading-[28px]">
+              Usually by snapping a picture of a specific cosplayer—with their
+              consent, of course
+            </p>
+          </div>
           {/* Bloco da esquerda */}
           <div
             className="h-[440px] w-full md:w-[50%] flex justify-center items-center"
             style={{
-              backgroundImage: "url(/lp/map2.png)",
-              backgroundSize: "cover",
-              backgroundPosition: "center",
+              backgroundImage: 'url(/lp/map2.png)',
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
             }}
-          >
-          </div>
+          ></div>
         </div>
       </div>
       <div className="flex flex-col space-y-4 items-center">
@@ -68,20 +70,25 @@ const HowItWorksSection: React.FC = () => {
           <div
             className="h-[440px] w-full md:w-[50%] flex justify-center items-center"
             style={{
-              backgroundImage: "url(/lp/map3.png)",
-              backgroundSize: "cover",
-              backgroundPosition: "center",
+              backgroundImage: 'url(/lp/map3.png)',
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
             }}
-          >
-          </div>
+          ></div>
           {/* Bloco da direita */}
-          <div className="h-[440px] w-full md:w-[50%] flex flex-col justify-center items-start bg-transparent p-8"
-            style={{background: "rgba(25, 159, 236, 0.15)"}}>
+          <div
+            className="h-[440px] w-full md:w-[50%] flex flex-col justify-center items-start bg-transparent p-8"
+            style={{ background: 'rgba(25, 159, 236, 0.15)' }}
+          >
             <h3 className="text-white text-[40px] font-bold mb-2 leading-tight">
-              Return to the Quest Giver to claim your badge ribbons... <span className='text-[#199FEC]'>and other possible rewards</span>
+              Return to the Quest Giver to claim your badge ribbons...{' '}
+              <span className="text-[#199FEC]">and other possible rewards</span>
             </h3>
             <p className="text-[rgba(255,255,255,0.5)] text-base mb-6 leading-[28px]">
-              Where we go from there is up to you. We can part ways. The story ends. You wake up in your bed and believe… whatever you want to believe. Or you stay in wonderland, and we show you how deep this rabbit hole goes…
+              Where we go from there is up to you. We can part ways. The story
+              ends. You wake up in your bed and believe… whatever you want to
+              believe. Or you stay in wonderland, and we show you how deep this
+              rabbit hole goes…
             </p>
           </div>
         </div>
