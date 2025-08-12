fe4da4ee67a1143499e58e4558fb6f8d446636cd
joaodes02 <joaovictordes02@gmail.com>
2025-08-12 20:50:32 -0300

    home section conquesting

--- src/components/LandingPage/HeroSection.tsx
+++ src/components/LandingPage/HeroSection.tsx
@@ -1,12 +1,13 @@
-import Image from 'next/image';
-import React from 'react';
-import Arrow from '../icons/arrow';
+import Image from "next/image";
+import React from "react";
+import Arrow from "../icons/arrow";
 
 const HeroSection: React.FC = () => {
   return (
     <>
+      {/*VERSÃO DESKTOP*/}
       <section
-        className="w-full bg-cover bg-center bg-gray-900 text-white flex flex-col md:px-20 gap-8 pt-20 md:pt-36"
+        className="hidden w-full bg-cover bg-center bg-gray-900 text-white md:flex flex-col md:px-20 gap-8 pt-20 md:pt-36"
         style={{ backgroundImage: `url('/lp/fundoHeroSection.png')` }}
       >
         <div className="flex items-start z-0 justify-start flex-row gap-[12px] px-[40px]">
@@ -60,124 +61,66 @@ const HeroSection: React.FC = () => {
             />
           </div>
         </div>
-        <div className="flex flex-col md:flex-row items-center md:items-start justify-between w-full gap-4 py-40 md:mt-0">
-          <div className="flex flex-col items-start gap-4 order-2 md:order-1 w-full md:w-auto">
-            <p className="text-2xl font-medium leading-10 tracking-tight">
-              Claim Your First Reward Here
+      </section>
+
+      {/*VERSÃO MOBILE*/}
+      <section className="w-full h-[880px] bg-[url('/lp/fundoHeroSection-mob.png')] bg-cover bg-center bg-gray-900 text-white flex flex-col md:px-20 gap-8 pt-20 md:pt-36">
+        <div className="flex items-start z-0 justify-start flex-row gap-3 px-6">
+          <span className="text-[rgba(255,255,255,0.5)] text-16px font-normal">
+            Home
+          </span>
+          <svg
+            className="justify-center items-center text-center flex"
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 20 12"
+            fill="none"
+          >
+            <path
+              d="M10.9764 10.0006L6.85156 5.87577L8.03008 4.69727L13.3334 10.0006L8.03008 15.3038L6.85156 14.1253L10.9764 10.0006Z"
+              fill="white"
+              fillOpacity="0.5"
+            />
+          </svg>
+          <span className="text-[16px]">ConQuesting</span>
+        </div>
+        <div className="flex flex-col md:flex-row items-center justify-between w-full gap-8">
+          <div className="w-full md:w-2/3 text-left order-2 px-[22px] pt-[39px]">
+            <h1 className="text-[60px] lg:text-[124px] leading-[56px] lg:leading-[108px] font-bold tracking-[-2px] lg:tracking-[-7.44px]">
+              Start Your <br className="hidden md:block" /> ConQuest
+            </h1>
+            <p className="text-[20px] leading-7 font-medium tracking-[-0.8px] w-full md:max-w-xl pt-6">
+              ConQuesting is a cosplay-focused photo-scavenger hunt where you
+              earn free rewards by tracking down cosplays from your favorite
+              fandoms. Prizes include badge ribbons, pins, and digital TTRPG
+              rulebooks.
             </p>
-            <div className="flex flex-col md:flex-row items-center justify-between w-full gap-4">
-              <div
-                className="relative w-full md:w-[416px] h-[268px] shrink-0 rounded-2xl border"
-                style={{
-                  borderColor:
-                    'var(--Transparent-secondary-T2-30, rgba(25, 159, 236, 0.30))',
-                }}
-              >
-                <div
-                  className="flex h-full w-full rounded-2xl"
-                  style={{
-                    background:
-                      'var(--bg-gradient-bottom-right, linear-gradient(111deg, rgba(25, 159, 236, 0.10) 0.9%, rgba(25, 159, 236, 0.55) 100%))',
-                  }}
-                >
-                  <div className="flex flex-col justify-between p-6 flex-1 gap-4">
-                    <p className="text-white text-lg font-medium leading-7">
-                      Get the Epic Quest Tome — quest book packed with
-                      adventures to choose from
-                    </p>
-                    <button className="btn w-fit">Download as PDF</button>
-                  </div>
-
-                  <div className="relative w-32 h-full rounded-r-2xl shrink-0">
-                    <Image
-                      src="/lp/poster.png"
-                      alt="ConQuest Poster"
-                      width={108}
-                      height={132}
-                      className="absolute inset-0 object-contain m-auto"
-                    />
-                  </div>
-                </div>
+            <div className="pt-9 w-[327px] flex flex-col items-center text-[16px] leading-6 font-normal">
+              <div className="flex ">
+                <span>Assemble your party</span>
+                <Arrow className="" />
+                <span>Select your quests</span>
+                <div className="flex flex-col"></div>
               </div>
-              <div
-                className="relative w-full md:w-[416px] h-[268px] shrink-0 rounded-2xl border"
-                style={{
-                  borderColor:
-                    'var(--Transparent-secondary-T2-30, rgba(25, 159, 236, 0.30))',
-                }}
-              >
-                <div
-                  className="flex h-full w-full rounded-2xl"
-                  style={{
-                    background:
-                      'var(--bg-gradient-bottom-right, linear-gradient(111deg, rgba(25, 159, 236, 0.10) 0.9%, rgba(25, 159, 236, 0.55) 100%))',
-                  }}
-                >
-                  <div className="flex flex-col justify-between p-6 flex-1 gap-4">
-                    <p className="text-white text-lg font-medium leading-7">
-                      Get the Epic Quest Tome — quest book packed with
-                      adventures to choose from
-                    </p>
-                    <button className="btn w-fit">Download as PDF</button>
-                  </div>
-
-                  <div className="relative w-32 h-full  rounded-r-2xl shrink-0">
-                    <Image
-                      src="/lp/poster.png"
-                      alt="ConQuest Poster"
-                      width={108}
-                      height={132}
-                      className="absolute inset-0 object-contain m-auto"
-                    />
-                  </div>
-                </div>
+              <div className="flex">
+                <Arrow />
+                <span>Snap your pics</span>
+                <Arrow />
+                <span>Save your spots</span>
+                <Arrow />
               </div>
             </div>
           </div>
-          <div className="flex w-full flex-col items-start gap-4 order-1 md:order-2">
-            <p className="text-2xl font-medium leading-10 tracking-tight">
-              Useful Links
-            </p>
-            <div className="flex flex-col items-center justify-between w-full gap-3">
-              <div className="relative h-[128px] w-full rounded-2xl p-[0.5px]">
-                <div
-                  className="relative flex flex-col justify-between h-full w-full rounded-2xl p-6"
-                  style={{
-                    background:
-                      'var(--bg-gradient-bottom-right, linear-gradient(111deg, rgba(25, 159, 236, 0.10) 0.9%, rgba(25, 159, 236, 0.55) 100%))',
-                  }}
-                >
-                  <p className="text-white text-sm font-medium leading-5 max-w-[50%] line-clamp-2">
-                    Follow us on Instagram for more loot and quests!
-                  </p>
-                  <button className="btn w-fit">Follow</button>
-                  <img
-                    src="/lp/instagram.svg"
-                    alt="Instagram"
-                    className="absolute bottom-0 right-4 h-full w-auto"
-                  />
-                </div>
-              </div>
-              <div className="relative h-[128px] w-full rounded-2xl p-[0.5px]">
-                <div
-                  className="relative flex flex-col justify-between h-full w-full rounded-2xl p-6"
-                  style={{
-                    background:
-                      'var(--bg-gradient-bottom-right, linear-gradient(111deg, rgba(25, 159, 236, 0.10) 0.9%, rgba(25, 159, 236, 0.55) 100%))',
-                  }}
-                >
-                  <p className="text-white text-sm font-medium leading-5 max-w-[50%] line-clamp-2">
-                    Join the ConQuesting community on the Echelon Discord Server
-                  </p>
-                  <button className="btn w-fit">Follow</button>
-                  <img
-                    src="/lp/discord.svg"
-                    alt="Discord"
-                    className="absolute bottom-0 right-4 h-full w-auto"
-                  />
-                </div>
-              </div>
-            </div>
+
+          <div className="w-full md:w-1/3 flex justify-center items-start order-1 md:order-2">
+            <Image
+              src="/lp/conquesting-logo.png"
+              alt="Conquesting Logo"
+              width={180}
+              height={180}
+              className="shrink-0"
+            />
           </div>
         </div>
       </section>
--- src/components/icons/arrow.tsx
+++ src/components/icons/arrow.tsx
@@ -1,4 +1,4 @@
-import { IconProps } from '../../utils/types';
+import { IconProps } from "../../utils/types";
 
 export function Arrow({ className, ...props }: IconProps) {
   return (
@@ -10,7 +10,10 @@ export function Arrow({ className, ...props }: IconProps) {
       className={className}
       {...props}
     >
-      <path d="M7.5 3.22427C8.16667 3.56904 8.16667 4.43096 7.5 4.77573L1.5 7.87866C0.833334 8.22343 6.63909e-07 7.79247 5.6647e-07 7.10293L1.33333 4L-3.10479e-07 0.897075C-4.07918e-07 0.207535 0.833333 -0.223427 1.5 0.121343L7.5 3.22427Z" />
+      <path
+        d="M7.5 3.22427C8.16667 3.56904 8.16667 4.43096 7.5 4.77573L1.5 7.87866C0.833334 8.22343 6.63909e-07 7.79247 5.6647e-07 7.10293L1.33333 4L-3.10479e-07 0.897075C-4.07918e-07 0.207535 0.833333 -0.223427 1.5 0.121343L7.5 3.22427Z"
+        fill="#199FEC"
+      />
     </svg>
   );
 }
