48460cd311c9f1866652588864b2bf7b3c125da4
Hugo Pereira <hugop868@gmail.com>
2025-08-13 11:56:31 -0300

    dsa

--- src/components/LandingPage/Reward.tsx
+++ src/components/LandingPage/Reward.tsx
@@ -11,68 +11,59 @@ export default function Reward() {
           </p>
           <div className="flex flex-col md:flex-row items-center justify-between w-full gap-4">
             <div
-              className="relative w-full md:w-[416px] h-[268px] shrink-0 rounded-2xl border"
+              className="p-[1px] rounded-xl w-full md:flex-1 md:w-[416px] h-[270px]"
               style={{
-                borderColor:
-                  'var(--Transparent-secondary-T2-30, rgba(25, 159, 236, 0.30))',
+                background: 'linear-gradient(rgba(25, 159, 236, 0.30))',
               }}
             >
-              <div
-                className="flex h-full w-full rounded-2xl"
-                style={{
-                  background:
-                    'var(--bg-gradient-bottom-right, linear-gradient(111deg, rgba(25, 159, 236, 0.10) 0.9%, rgba(25, 159, 236, 0.55) 100%))',
-                }}
-              >
-                <div className="flex flex-col justify-between p-6 flex-1 gap-4">
-                  <p className="text-white text-lg font-medium leading-7">
-                    Get the Epic Quest Tome — quest book packed with adventures
-                    to choose from
-                  </p>
-                  <button className="btn w-fit">Download as PDF</button>
-                </div>
+              <div className="relative w-full h-[268px] shrink-0 rounded-xl bg-black overflow-hidden">
+                <div
+                  className="flex h-full w-full rounded-xl"
+                  style={{
+                    background:
+                      'var(--bg-gradient-bottom-right, linear-gradient(111deg, rgba(25, 159, 236, 0.10) 0.9%, rgba(25, 159, 236, 0.55) 100%))',
+                  }}
+                >
+                  <div className="w-2/3 relative flex flex-col justify-between pr-2 p-6 gap-4">
+                    <div className="z-10 flex flex-col items-start gap-2">
+                      <p className="text-white text-lg font-medium leading-7">
+                        Get the Epic Quest Tome — quest book packed with
+                        adventures to choose from
+                      </p>
+                    </div>
 
-                <div className="relative w-32 h-full rounded-r-2xl shrink-0">
-                  <Image
-                    src="/lp/poster.png"
-                    alt="ConQuest Poster"
-                    width={108}
-                    height={132}
-                    className="absolute inset-0 object-contain m-auto"
-                  />
+                    <button className="btn w-fit">Download as PDF</button>
+                  </div>
+
+                  <div className="absolute right-0 bg-cover bg-center w-1/2 h-full bg-[url('/lp/follow-001.png')] rounded-r-2xl shrink-0"></div>
                 </div>
               </div>
             </div>
             <div
-              className="relative w-full md:w-[416px] h-[268px] shrink-0 rounded-2xl border"
+              className="p-[1px] rounded-xl w-full md:flex-1 md:w-[416px] h-[270px]"
               style={{
-                borderColor:
-                  'var(--Transparent-secondary-T2-30, rgba(25, 159, 236, 0.30))',
+                background: 'linear-gradient(rgba(25, 159, 236, 0.30))',
               }}
             >
-              <div
-                className="flex h-full w-full rounded-2xl"
-                style={{
-                  background:
-                    'var(--bg-gradient-bottom-right, linear-gradient(111deg, rgba(25, 159, 236, 0.10) 0.9%, rgba(25, 159, 236, 0.55) 100%))',
-                }}
-              >
-                <div className="flex flex-col justify-between p-6 flex-1 gap-4">
-                  <p className="text-white text-lg font-medium leading-7">
-                    Get the Epic Quest Tome — quest book packed with adventures
-                    to choose from
-                  </p>
-                  <button className="btn w-fit">Download as PDF</button>
-                </div>
+              <div className="relative w-full h-[268px] shrink-0 rounded-xl bg-black overflow-hidden">
+                <div
+                  className="flex h-full w-full rounded-xl"
+                  style={{
+                    background:
+                      'var(--bg-gradient-bottom-right, linear-gradient(111deg, rgba(25, 159, 236, 0.10) 0.9%, rgba(25, 159, 236, 0.55) 100%))',
+                  }}
+                >
+                  <div className="w-2/3 relative flex flex-col justify-between pr-2 p-6 gap-4">
+                    <div className="z-10 flex flex-col items-start gap-2">
+                      <p className="text-white text-lg font-medium leading-7">
+                        Anno Amagium: The RPG - Core Rules
+                      </p>
+                    </div>
 
-                <div className="relative w-32 h-full  rounded-r-2xl shrink-0">
-                  <Image
-                    src="/lp/poster.png"
-                    alt="ConQuest Poster"
-                    width={108}
-                    height={132}
-                    className="absolute inset-0 object-contain m-auto"
-                  />
+                    <button className="btn w-fit">Download as PDF</button>
+                  </div>
+
+                  <div className="absolute right-0 bg-cover bg-center w-1/2 h-full bg-[url('/lp/follow-001.png')] rounded-r-2xl shrink-0"></div>
                 </div>
               </div>
             </div>
@@ -83,42 +74,72 @@ export default function Reward() {
             Useful Links
           </p>
           <div className="flex flex-col items-center justify-between w-full gap-3">
-            <div className="relative h-[128px] w-full rounded-2xl p-[0.5px]">
-              <div
-                className="relative flex flex-col justify-between h-full w-full rounded-2xl p-6"
-                style={{
-                  background:
-                    'var(--bg-gradient-bottom-right, linear-gradient(111deg, rgba(25, 159, 236, 0.10) 0.9%, rgba(25, 159, 236, 0.55) 100%))',
-                }}
-              >
-                <p className="text-white text-sm font-medium leading-5 max-w-[50%] line-clamp-2">
-                  Follow us on Instagram for more loot and quests!
-                </p>
-                <button className="btn w-fit">Follow</button>
-                <img
-                  src="/lp/instagram.svg"
-                  alt="Instagram"
-                  className="absolute bottom-0 right-4 h-full w-auto"
-                />
+            <div
+              className="p-[1px] rounded-xl w-full md:flex-1 md:w-[416px] h-[128px]"
+              style={{
+                background:
+                  'linear-gradient(130deg, #002C45 0%, #F45111 35%, #199FEC 70%, #002C45 100%)',
+              }}
+            >
+              <div className="relative w-full h-[128px] shrink-0 rounded-xl bg-black overflow-hidden">
+                <div
+                  className="flex h-full w-full rounded-xl"
+                  style={{
+                    background:
+                      'var(--bg-gradient-bottom-right, linear-gradient(111deg, rgba(25, 159, 236, 0.10) 0.9%, rgba(25, 159, 236, 0.55) 100%))',
+                  }}
+                >
+                  <div className="w-full relative flex flex-col justify-between pr-2 p-6 gap-4">
+                    <div className="z-10 flex flex-col items-start gap-2">
+                      <p className="text-white text-sm font-medium leading-5 max-w-[50%] line-clamp-2">
+                        Follow us on Instagram for more loot and quests!
+                      </p>
+                    </div>
+
+                    <button className="btn w-fit">Follow</button>
+                  </div>
+
+                  <img
+                    src="/lp/instagram.svg"
+                    alt="Instagram"
+                    className="absolute bottom-0 right-4 h-full w-auto"
+                  />
+                </div>
               </div>
             </div>
-            <div className="relative h-[128px] w-full rounded-2xl p-[0.5px]">
-              <div
-                className="relative flex flex-col justify-between h-full w-full rounded-2xl p-6"
-                style={{
-                  background:
-                    'var(--bg-gradient-bottom-right, linear-gradient(111deg, rgba(25, 159, 236, 0.10) 0.9%, rgba(25, 159, 236, 0.55) 100%))',
-                }}
-              >
-                <p className="text-white text-sm font-medium leading-5 max-w-[50%] line-clamp-2">
-                  Join the ConQuesting community on the Echelon Discord Server
-                </p>
-                <button className="btn w-fit">Follow</button>
-                <img
-                  src="/lp/discord.svg"
-                  alt="Discord"
-                  className="absolute bottom-0 right-4 h-full w-auto"
-                />
+
+            <div
+              className="p-[1px] rounded-xl w-full md:flex-1 md:w-[416px] h-[128px]"
+              style={{
+                background:
+                  'linear-gradient(130deg, #002C45 0%, #F45111 35%, #199FEC 70%, #002C45 100%)',
+              }}
+            >
+              <div className="relative w-full h-[128px] shrink-0 rounded-xl bg-black overflow-hidden">
+                <div
+                  className="flex h-full w-full rounded-xl"
+                  style={{
+                    background:
+                      'var(--bg-gradient-bottom-right, linear-gradient(111deg, rgba(25, 159, 236, 0.10) 0.9%, rgba(25, 159, 236, 0.55) 100%))',
+                  }}
+                >
+                  <div className="w-full relative flex flex-col justify-between pr-2 p-6 gap-4">
+                    <div className="z-10 flex flex-col items-start gap-2">
+                      <p className="text-white text-sm font-medium leading-5 max-w-[50%] line-clamp-2">
+                        Join the ConQuesting community on the Echelon Discord
+                        Server
+                      </p>
+                    </div>
+
+                    <button className="btn w-fit">Follow</button>
+                  </div>
+
+                  <img
+                    src="/lp/discord.svg"
+                    alt="Discord"
+                    className="absolute bottom-0 right-4 h-full w-auto"
+                  />
+                </div>
               </div>
             </div>
           </div>
@@ -126,12 +147,12 @@ export default function Reward() {
       </div>
 
       {/*VERSÃO MOBILE*/}
-      <div className="flex flex-col md:flex-row items-center justify-between w-full p-6 md:hidden ">
+      <div className="flex flex-col md:flex-row items-center justify-between w-full p-6 md:hidden bg-pink-900">
         <div className="flex w-full flex-col items-start">
           <p className="text-[28px] font-medium leading-9 tracking-[-1.12px]">
             Useful Links
           </p>
-          <div className="bg-pink-500 p-[1px] rounded-xl">
+          <div className="p-[1px] rounded-xl">
             <div
               className="flex flex-col items-center justify-between w-full gap-4 pt-6 rounded-xl"
               style={{
@@ -180,6 +201,7 @@ export default function Reward() {
             </div>
           </div>
         </div>
+
         <div className="flex flex-col items-start w-full pt-[52px]">
           <p className="text-[28px] font-medium leading-9 tracking-[-1.12px] pb-6">
             Claim Your First Reward Here
